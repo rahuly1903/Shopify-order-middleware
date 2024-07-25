ca09fc9bf5e6f47641a05783827f27609fc5121b
Rahul Yadav <rahulyadav@LT2060.local>
2024-07-25 16:13:26 +0530

    Added email notification message

--- webhook_order_data/order_data.js
+++ webhook_order_data/order_data.js
@@ -1,8 +1,30 @@
 const axios = require("axios");
 const https = require("https");
+const nodemailer = require("nodemailer");
+const { type } = require("os");
+
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+  port: 465, // true for 465, false for other ports
+  host: "smtp.gmail.com",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+  secure: true,
+});
+
 function order_data(data) {
   console.log("order_data");
+  const { name } = JSON.parse(data);
   console.log(process.env.BASE_URL);
+  const mailData = {
+    from: process.env.EMAIL, // sender address
+    to: process.env.RECEIVER, // list of receivers
+    subject: `Order - [${name}] Failed to send vportal`,
+    text: "",
+    html: `Order - <b>${name}</b> failed to send vportal`,
+  };
   try {
     let config = {
       method: "post",
@@ -20,10 +42,32 @@ function order_data(data) {
       .request(config)
       .then((response) => {
         console.log("sendStatus 200");
+        console.log("Response data: ", response.data);
+        console.log("Response status: ", response.status);
+        console.log("Response headers: ", response.headers);
       })
       .catch((error) => {
-        console.log(error);
-        console.log("sendStatus 500");
+        transporter.sendMail(mailData, function (err, info) {
+          if (err) {
+            res.send({ msg: `Error in sending Mail - ${err}` });
+          } else {
+            res.send({ msg: `mail send successfully` });
+          }
+        });
+        if (error.response) {
+          // The request was made, and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("sendStatus " + error.response.status);
+          // console.log("Error message: " + error.response.data);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log("sendStatus 500");
+          console.log("Error message: No response received");
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("sendStatus 500");
+          // console.log("Error message: " + error.message);
+        }
       });
   } catch (error) {
     console.error(error);
